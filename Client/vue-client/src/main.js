c1f3d83c7c85cabd91c44745fcc870488cbe2ccc
Anton Wieslander <atwieslander@gmai.com>
2019-10-20 00:46:44 +0100

    service worker setup

--- Client/vue-client/src/main.js
+++ Client/vue-client/src/main.js
@@ -4,11 +4,12 @@ import vuetify from './plugins/vuetify';
 import router from './router'
 import {store} from './stores/store'
 import "./components/shared/index"
+import "./registerServiceWorker"
 import Axios from 'axios'
+import Logger from "./logger/logger";
 
 import {loadProgressBar} from 'axios-progress-bar'
 import 'axios-progress-bar/dist/nprogress.css'
-import Logger from "./logger/logger";
 
 loadProgressBar();
 
--- Client/vue-client/vue.config.js
+++ Client/vue-client/vue.config.js
@@ -1,5 +1,22 @@
 ﻿module.exports = {
     devServer: {
         public: 'https://localhost:8080'
+    },
+    pwa: {
+        name: 'Tricking Royal',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: '#9a48ab',
+        msTileColor: '#9a48ab',
+        themeColor: '#9a48ab',
+        iconPaths: {
+            favicon16: 'https://cdn.trickingroyal.com/static/main_16x16.png',
+            favicon32: 'https://cdn.trickingroyal.com/static/main_32x32.png',
+            appleTouchIcon: 'https://cdn.trickingroyal.com/static/main_152x152.png',
+            msTileImage: 'https://cdn.trickingroyal.com/static/main_144x144.png'
+        },
+        workboxOptions: {
+            cacheId: 'tr-v1',
+            skipWaiting: true,
+        },
     }
-}
+};
